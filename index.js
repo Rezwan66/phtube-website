89d5af38ff65be8db981592397f060ecb9d8da6a
Rezwan66 <shaikhrezwan66@gmail.com>
2023-09-01 02:19:56 +0600

    fetched and shown the videos by category

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const handleCategory = async () => {
     categories.forEach(category => {
         const div = document.createElement('div');
         div.innerHTML = `
-        <a class="tab btn px-6 bg-[#FF1F3D] normal-case text-white hover:bg-red-400">
+        <a onclick="handleLoadVideos('${category.category_id}')" class="tab btn px-6 bg-[#FF1F3D] normal-case text-white hover:bg-red-400">
             ${category.category}
         </a>
         `;
@@ -17,8 +17,39 @@ const handleCategory = async () => {
     })
 }
 
-const handleLoadVideos = async (categoryId) => {
+const handleLoadVideos = async categoryId => {
     const res = await fetch(`https://openapi.programming-hero.com/api/videos/category/${categoryId}`);
+    const data = await res.json();
+    console.log(data.data);
+    const videosArr = data.data; // this is my videos array
+
+    const cardContainer = document.getElementById('card-container'); // this is my empty cards container
+    cardContainer.textContent = ''; // reload everytime for each category
+
+    videosArr?.forEach(video => {
+        const div = document.createElement('div');
+        div.innerHTML = `
+            <div class="card card-compact bg-base-100">
+                <figure><img src="./images/Icon.png" alt="Shoes" class="rounded-lg" /></figure>
+                <div class="badge badge-md bg-[#171717] text-white -mt-4">987,654</div>
+                <div class="card-body">
+                    <div class="flex justify-center gap-2">
+                        <div>
+                            <figure><img src="./images/Icon.png" alt="Shoes" class="w-14 rounded-full" /></figure>
+                        </div>
+                        <div class="space-y-2">
+                            <h2 class="text-base font-bold">If a dog chews shoes whose shoes does he choose?</h2>
+                            <p>Shoes!</p>
+                            <!-- svg here -->
+                            <p>views</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        `;
+        cardContainer.appendChild(div);
+    })
 }
 
 handleCategory();
+handleLoadVideos('1000');
