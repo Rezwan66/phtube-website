09ada26c131c8ba33e1b944968c28ac85afb6e2c
Rezwan66 <shaikhrezwan66@gmail.com>
2023-09-01 00:53:43 +0600

    fetched and loaded the categories to the html doc

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log('connected');
+// console.log('connected');
+const handleCategory = async () => {
+    const res = await fetch('https://openapi.programming-hero.com/api/videos/categories');
+    const data = await res.json();
+    // console.log(data.data);
+    const categories = data.data; // this is my categories array
+    const tabContainer = document.getElementById('tab-container'); // this is my empty tab container
+    tabContainer.textContent = '';
+    categories.forEach(category => {
+        const div = document.createElement('div');
+        div.innerHTML = `
+        <a class="tab btn px-6 bg-[#FF1F3D] normal-case text-white hover:bg-red-400">
+            ${category.category}
+        </a>
+        `;
+        tabContainer.appendChild(div);
+    })
+}
+
+handleCategory();
